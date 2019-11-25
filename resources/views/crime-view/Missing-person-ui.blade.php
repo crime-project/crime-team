a82bd3c8e8ccb8d9c1e9d42be7d0ec30853482b8
devinda <dark25@gmail.comgit config --global user.name devindaconfig --listgit config --global user.email dark25@gmail.com>
2019-11-25 17:22:57 +0530

    modal

--- resources/views/crime-view/Missing-person-ui.blade.php
+++ resources/views/crime-view/Missing-person-ui.blade.php
@@ -21,7 +21,7 @@
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
    <!-- <a href="#" class="btn btn-primary">Go somewhere</a>-->
 
-   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Large modal</button>
+   <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
   </div>
 </div>
 
@@ -151,28 +151,134 @@
 
 
 
-<!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+<!-- Large modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
 
-<!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-       
-        <h4 class="modal-title">Modal Header</h4>
-         <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
 
+<h4 class="modal-title">Modal Header</h4>
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+</div>
+     
+        <!-- Right Panel -->
+<div class="col-lg-12">  
+ <!--<div id="right-panel" class="right-panel">-->
+
+    <div class="container-fluid missing-person-container">
+
+        <!--<div class="col-12">-->
+            <div class="card Missing-person">
+                <h4><strong><div class="card-header text-center bg-info">Missing Persons</div></strong></h4>
+
+                <div class="card-body">
+                    <form method="POST" action="http://127.0.0.1:8000/register">
+                        <input type="hidden" name="_token" value="yV21wMIYxCTasrhiPledHtezmlYqERn0aoJHCXkl">
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Full name </label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control " name="name" value="" required="" autocomplete="name" autofocus="">
+
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control " name="email" value="" required="" autocomplete="email">
+
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Phone Number</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control " name="password" required="" autocomplete="new-password">
+
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Gender</label>
+
+                            <div class="col-md-6">
+                              <div class="custom-control custom-radio custom-control-inline">
+                                  <input type="radio" class="custom-control-input" id="defaultInline1" name="inlineDefaultRadiosExample">
+                                  <label class="custom-control-label" for="defaultInline1">Male</label>
+                              </div>
+
+                              <div class="custom-control custom-radio custom-control-inline">
+                                  <input type="radio" class="custom-control-input" id="defaultInline2" name="inlineDefaultRadiosExample">
+                                  <label class="custom-control-label" for="defaultInline2">Female</label>
+                              </div>
+                          </div>
+                      </div>
+
+
+
+                      <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">Image</label>
+
+                        <div class="col-md-6">
+                            <div class="input-group">
+                              <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+                            </div>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                                <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">Description</label>
+
+                    <div class="col-md-6">
+                        <div class="form-group">
+
+                          <textarea class="form-control" id="exampleFormControlTextarea3" rows="7"></textarea>
+                      </div>
+
+                  </div>
+              </div>
+
+
+
+              <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-9">
+
+                  <h4><button type="button" class="btn btn-info rounded">
+
+                    Submit
+                </button></h4>
+            </div>
+        </div>
+    </form>
+</div>
+</div>
+<!-- </div>-->
+
+</div>
+
+<!--</div>-->
+</div>
+
+
+    </div>
   </div>
 </div>
 
@@ -184,3 +290,8 @@
 
 
 @endsection
+
+
+
+
+
