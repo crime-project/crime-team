260cfd5b60c51e43d3b4b514e779c73f82a388f7
Mayuri <52152678+Mayu06@users.noreply.github.com>
2019-11-26 21:34:47 +0530

    Commit

---
+++ database/migrations/2019_11_26_155718_create_vehicles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('own_name');
+            $table->string('mod_number'); 
+            $table->string('num_plate');
+            $table->string('address');
+            $table->smallInteger('phone_number');
+            $table->binary('image');
+            $table->longtext('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
