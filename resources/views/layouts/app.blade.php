c8c34b244c621e84021e69d3ae14d483cdee4ff0
devinda <dark25@gmail.comgit config --global user.name devindaconfig --listgit config --global user.email dark25@gmail.com>
2019-11-28 11:18:57 +0530

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Crime') }}</title>
 
     <!-- Scripts -->
-  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -41,9 +41,9 @@
 <!-- Styles for datepicker-->
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
 
-   <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+  <!-- <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
    
   <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 
@@ -182,7 +182,7 @@ $('li.dropdown-alert').hover(function() {
     <script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
 
     <!--datapicker-->
-        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        <!--<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         
         <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
           
@@ -195,7 +195,7 @@ $('li.dropdown-alert').hover(function() {
 
 <!--dashboaed-js-links-end-->
 
- <!--<script src="{{ asset('js/app.js') }}" defer></script>-->s
+ <script src="{{ asset('js/app.js') }}" defer></script>
 
 </body>
 </html>
