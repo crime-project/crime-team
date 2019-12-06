10d8e3f493fb92c9545553bd6fea16f8d392b01f
Mayuri <52152678+Mayu06@users.noreply.github.com>
2019-12-06 14:56:53 +0530

    Commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,17 @@
                             </div>
                         </div>
 
+                                        <div class="form-group row">
+                                            <label for="profile_image" class="col-md-4 col-form-label text-md-right">Profile Image</label>
+                                            <div class="col-md-6">
+                                                <input id="profile_image" type="file" class="form-control" name="profile_image">
+                                                @if (auth()->user()->image)
+                                                    <code>{{ auth()->user()->image }}</code>
+                                                @endif
+                                            </div>
+                                        </div>
+
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
